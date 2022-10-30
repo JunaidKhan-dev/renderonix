b6e71cb93730c79f82d07b309f9b4c6623a8f9fe
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-10-30 15:41:33 +0000

    so far storybook needs looking

--- src/stories/components/Button/button.style.ts
+++ src/stories/components/Button/button.style.ts
@@ -1,7 +1,7 @@
 import styled, { css } from "styled-components";
 import { ButtonVariants, ButtonSizes} from "../../../types/index"
 
-interface WrapperProps: {
+interface WrapperProps {
   variant: ButtonVariants
   size: ButtonSizes
 }
